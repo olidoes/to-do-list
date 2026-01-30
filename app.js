4292d8c37d056140b08bb4e4664ecc926958add8
Oli <palmbusiness12@gmail.com>
2026-01-30 10:09:37 +0000

    Fully styled and basic functionality is added.

---
+++ app.js
@@ -0,0 +1,54 @@
+const toDos = [];
+
+function loadToDos() {
+    for (let i = 0; i < toDos.length; i++) {
+        if (toDos[i].added === true) {
+            continue;
+        }
+        if (toDos[i].value === "") {
+            continue;
+        }
+        if (toDos[i].done === true) {
+            delete toDos[i];
+        } else {
+            const toDosContiner = document.querySelector(
+                ".to-do-list-container",
+            );
+            let p = document.createElement("p");
+            p.innerText = toDos[i].value;
+            p.className = `toDo-${toDos[i].value}`;
+
+            toDosContiner.appendChild(p);
+
+            toDos[i].added = true;
+        }
+    }
+}
+
+function handleAddButtonClick() {
+    const inputFieldValue = document.getElementById("input-area").value;
+    const newToDo = toDos.push({
+        value: inputFieldValue,
+        done: false,
+        added: false,
+    });
+
+    loadToDos();
+}
+
+function handleToDoClick(event) {
+    for (const obj of toDos) {
+        if (`toDo-${obj.value}` === event.target.className) {
+            delete toDos[obj];
+            document.querySelector(`.toDo-${obj.value}`).remove();
+        }
+    }
+}
+
+document
+    .getElementById("input-button")
+    .addEventListener("click", handleAddButtonClick);
+
+document
+    .querySelector(".to-do-list-container")
+    .addEventListener("click", (event) => handleToDoClick(event));
